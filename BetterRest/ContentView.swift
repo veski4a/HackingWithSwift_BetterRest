53be75b3c64316c1e45c6083081356eb78c71f2e
Veselin Stefanov <veski4a92@gmail.com>
2020-09-18 08:12:02 +0300

    Cleaning the UI

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date()
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
@@ -20,28 +20,36 @@ struct ContentView: View {
     var body: some View {
         
         NavigationView{
-            VStack{
-                Text("When do you want to wake up?")
-                    .font(.headline)
-                
-                DatePicker("Pleaser enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                
-                Stepper(value: $sleepAmount, in: 4...12, step: 0.25){
-                    Text("\(sleepAmount, specifier: "%g") hours")
+            Form{
+                VStack(alignment: .leading, spacing: 0){
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Pleaser enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
                 }
                 
-                Text("Daily coffee intake")
-                
-                Stepper(value: $coffeeAmount, in: 1...20){
-                    if coffeeAmount == 1{
-                        Text("1 cup")
+                VStack(alignment: .leading, spacing: 0){
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper(value: $sleepAmount, in: 4...12, step: 0.25){
+                        Text("\(sleepAmount, specifier: "%g") hours")
                     }
-                    else {
-                        Text("\(coffeeAmount) cups")
+                }
+                
+                VStack(alignment: .leading, spacing: 0){
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Stepper(value: $coffeeAmount, in: 1...20){
+                        if coffeeAmount == 1{
+                            Text("1 cup")
+                        }
+                        else {
+                            Text("\(coffeeAmount) cups")
+                        }
                     }
                 }
             }
@@ -55,6 +63,14 @@ struct ContentView: View {
                 Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
             }
         }
+    }
+    
+    static var defaultWakeTime: Date{
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        
+        return Calendar.current.date(from: components) ?? Date()
     }
     
     private func calculateBedtime(){
